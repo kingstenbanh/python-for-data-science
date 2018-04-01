e3a56f6fcc7ad64eedbec95841da76005c87a041
Kingsten Banh <kingstenbanh@gmail.com>
2018-03-31 23:47:04 -0700

    Compare among different sklearn classifiers

--- gender-classifier.py
+++ gender-classifier.py
@@ -1,6 +1,11 @@
 from sklearn import tree
+from sklearn.svm import SVC
+from sklearn.linear_model import Perceptron
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+import numpy as np
 
-# [height, weight, shoe size]
+# Data and labels
 X = [
     [181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37],
     [166, 65, 40], [190, 90, 47], [175, 64, 39], [177, 70, 40],
@@ -13,10 +18,37 @@ Y = [
     'female', 'male', 'male'
 ]
 
-clf = tree.DecisionTreeClassifier()
+# Classifiers
+# using the default values for all the hyperparameters
+clf_tree = tree.DecisionTreeClassifier()
+clf_svm = SVC()
+clf_perceptron = Perceptron()
+clf_KNN = KNeighborsClassifier()
 
-clf = clf.fit(X, Y)
+# Training the models
+clf_tree = clf_tree.fit(X, Y)
+clf_svm = clf_svm.fit(X, Y)
+clf_perceptron = clf_perceptron.fit(X, Y)
+clf_KNN = clf_KNN.fit(X, Y)
 
-prediction = clf.predict([[190, 70, 43]])
+# Testing using the same data
+pred_tree = clf_tree.predict(X)
+acc_tree = accuracy_score(Y, pred_tree) * 100
+print('Accuracy for DecisionTree: {}'.format(acc_tree))
 
-print(prediction)
+pred_svm = clf_svm.predict(X)
+acc_svm = accuracy_score(Y, pred_svm) * 100
+print('Accuracy for SVM: {}'.format(acc_svm))
+
+pred_per = clf_perceptron.predict(X)
+acc_per = accuracy_score(Y, pred_per) * 100
+print('Accuracy for perceptron: {}'.format(acc_per))
+
+pred_KNN = clf_KNN.predict(X)
+acc_KNN = accuracy_score(Y, pred_KNN) * 100
+print('Accuracy for KNN: {}'.format(acc_KNN))
+
+# The best classifier from svm, per, KNN
+index = np.argmax([acc_svm, acc_per, acc_KNN])
+classifiers = { 0: 'SVM', 1: 'Perceptron',  2: 'KNN' }
+print('Best gender classifier is {}'.format(classifiers[index]))
